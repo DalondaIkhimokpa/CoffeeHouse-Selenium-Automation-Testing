c6c6d8a3a7392eca9d54646cd662d912254d06c7
DalondaIkhimokpa <primeops2006@gmail.com_>
2025-06-14 08:49:22 -0500

    Update Tests

--- tests/selenium/basic.test.js
+++ tests/selenium/basic.test.js
@@ -1,3 +1,4 @@
+const BASE_URL = process.env.CI ? 'http://localhost:8080' : 'http://localhost:8080';
 const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const fs = require('fs');
